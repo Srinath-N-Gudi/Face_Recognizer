a4220ffc32cadb1a7b1dbe795857961a77e7a2a2
Srinath <srinathngudi11@gmail.com>
2021-03-18 08:15:01 -0500

    Initial Commit

---
+++ Photo Taker.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import time
+faceData = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+video = cv2.VideoCapture(0)
+name = input("Enter your name: ")
+time.sleep(3)
+print("Started")
+if not os.path.exists(name):    
+    os.mkdir(name)
+os.chdir(os.getcwd()+f"/{name}")
+# print("Please wait")
+i = 1
+while True:
+    ret, frame = video.read()
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceData.detectMultiScale(gray_img, 1.1, 4)
+    for (x, y, w, h) in faces:
+        
+        # cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        face = frame[y:y+h, x:x+w]
+        if i < 11:
+            cv2.imwrite(str(i)+".png", face)
+            i=i+1
+        else:
+            exit()
+    # cv2.imshow("Window",frame)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+video.release()
+cv2.destroyAllWindows
